3a34f9df789c0cc96ac8556abffc697d56949f9a
dhanashree512 <dhanashreelale512@gmail.com>
2025-07-11 12:20:18 -0500

    completed strings 2

---
+++ findAllAnagrams.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> result = new ArrayList<>();
+        int n = p.length();
+        int m = s.length();
+
+        if(n > m) return result;
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(int i = 0; i<n; i++){
+            char c = p.charAt(i);
+            map.put(c, map.getOrDefault(c,0)+1);
+        }
+        int match = 0;
+        for(int i = 0;i<m;i++){
+            //in
+            char in = s.charAt(i);
+            if(map.containsKey(in)){
+                int cnt = map.get(in);
+                cnt--;
+                if(cnt == 0) match++;
+                map.put(in, cnt);
+            }
+
+            //out
+            if(i >= n){
+                char out = s.charAt(i-n);
+                if(map.containsKey(out)){
+                    int cnt = map.get(out);
+                    cnt++;
+                    if(cnt == 1) match--;
+                    map.put(out, cnt);
+                }
+            }
+            if(match == map.size()){
+                result.add(i-n+1);
+            }
+        }
+        return result;
+    }
+}
---
+++ strStr.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int m = haystack.length();
+        int n = needle.length();
+        if(n > m) return -1;
+        int i =0, j=0;
+        while(i <= m-n){
+            if(haystack.charAt(i) == needle.charAt(j)){
+                int k = i;
+                while(haystack.charAt(k) == needle.charAt(j)){
+                    k++;
+                    j++;
+                    if(j ==n){
+                        return i;
+                    }
+                }
+                j =0;
+            
+            }
+            i++;
+        }
+        return -1;
+    }
+}
